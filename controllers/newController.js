be740ca5554aabcb17384089e08a2403653667b8
muahchee <187490843+muahchee@users.noreply.github.com>
2025-10-17 23:56:16 +1300

    Build: add items

--- controllers/newController.js
+++ controllers/newController.js
@@ -15,16 +15,16 @@ export async function addItemPost(req, res) {
     sourceid1,
     sourceid2,
   } = req.body;
-  console.log(req.body)
+
   await addItem({
     name: name,
-    buyprice: Number(buyprice) || null,
-    sellprice: Number(sellprice) || null,
-    themeid1: Number(themeid1) || null,
-    themeid2: Number(themeid2) || null,
-    sourceid1: Number(sourceid1) || null,
-    sourceid2: Number(sourceid2) || null,
+    buyprice: buyprice === "" ? null : Number(buyprice),
+    sellprice: sellprice === "" ? null : Number(sellprice),
+    themeid1: themeid1 === "" ? null : Number(themeid1),
+    themeid2: themeid2 === "" ? null : Number(themeid2),
+    sourceid1: sourceid1 === "" ? null : Number(sourceid1),
+    sourceid2: sourceid2 === "" ? null : Number(sourceid2),
   });
-  
-  res.redirect("/")
+
+  res.redirect("/");
 }
--- controllers/updateController.js
+++ controllers/updateController.js
@@ -17,12 +17,12 @@ export async function updateItemPost(req, res) {
   } = req.body;
   await updateItem({
     name: name,
-    buyprice: buyprice,
-    sellprice: sellprice,
-    themeid1: themeid1,
-    themeid2: themeid2,
-    sourceid1: sourceid1,
-    sourceid2: sourceid2,
+    buyprice: buyprice === "" ? null : Number(buyprice),
+    sellprice: sellprice === "" ? null : Number(sellprice),
+    themeid1: themeid1 === "" ? null : Number(themeid1),
+    themeid2: themeid2 === "" ? null : Number(themeid2),
+    sourceid1: sourceid1 === "" ? null : Number(sourceid1),
+    sourceid2: sourceid2 === "" ? null : Number(sourceid2),
   });
 
   res.redirect("/");
